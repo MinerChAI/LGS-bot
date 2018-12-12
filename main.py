342146c830312bb83e7aecade662d43f92a898c6
Miner ChAI <43097806+MinerChAI@users.noreply.github.com>
2018-12-12 20:57:43 +0300

    lgs.addroles и removeroles

--- main.py
+++ main.py
@@ -18,5 +18,20 @@ async def on_message(message: discord.Message):
             for i in message.channel_mentions:
                 await client.send_message(i, text)
         await client.send_message(message.channel, text)
+    
+    if message.content.lower().startswith('lgs.zdku'):
+        await client.send_message(message.channel, message.mentions[0].server_permissions.administrator)
+    
+    if message.content.lower().startswith('lgs.addroles') and message.author.server_permissions.manage_roles:
+        roles = [i for i in message.role_mentions if message.author.top_role >= i]
+        for i in message.mentions:
+            await client.add_roles(i, *roles)
+        await client.send_message(message.channel, 'Роли ' + ', '.join([i.mention for i in message.role_mentions]) + ' добавлены к участникам ' + ', '.join([i.mention for i in message.mentions]))
+    
+    if message.content.lower().startswith('lgs.removeroles') and message.author.server_permissions.manage_roles:
+        roles = [i for i in message.role_mentions if message.author.top_role >= i]
+        for i in message.mentions:
+            await client.remove_roles(i, *roles)
+        await client.send_message(message.channel, 'Роли ' + ', '.join([i.mention for i in message.role_mentions]) + ' сняты с участников ' + ', '.join([i.mention for i in message.mentions]))
 
 client.run(token)
