cfabe970df8440d95fe1e6aca2ae617815087c4a
Miner ChAI <43097806+MinerChAI@users.noreply.github.com>
2018-12-12 21:25:15 +0300

    Исправил баг; изменил цвет `lgs.help`

--- main.py
+++ main.py
@@ -13,31 +13,32 @@ async def on_ready():
 async def on_message(message: discord.Message):
     print(message.server, '/',  message.channel, '/', message.author, 'написал', message.content)    
     if message.content.lower().startswith('lgs.'):
-        cmd = ' '.join(message.content[4:])    
-        if message.content.lower().startswith('say'):
+        print('LOL')
+        cmd = message.content[4:]    
+        if cmd.startswith('say'):
             text = message.content.split('|', 1)[1]
             if message.channel_mentions:
                 for i in message.channel_mentions:
                     await client.send_message(i, text)
             await client.send_message(message.channel, text)
     
-        if message.content.lower().startswith('zdku'):
+        if cmd.startswith('zdku'):
             await client.send_message(message.channel, message.mentions[0].server_permissions.administrator)
     
-        if message.content.lower().startswith('addroles') and message.author.server_permissions.manage_roles:
+        if cmd.startswith('addroles') and message.author.server_permissions.manage_roles:
             roles = [i for i in message.role_mentions if message.author.top_role >= i]
             for i in message.mentions:
                 await client.add_roles(i, *roles)
             await client.send_message(message.channel, 'Роли ' + ', '.join([i.mention for i in message.role_mentions]) + ' добавлены к участникам ' + ', '.join([i.mention for i in message.mentions]))
     
-        if message.content.lower().startswith('removeroles') and message.author.server_permissions.manage_roles:
+        if cmd.startswith('removeroles') and message.author.server_permissions.manage_roles:
             roles = [i for i in message.role_mentions if message.author.top_role >= i]
             for i in message.mentions:  
                 await client.remove_roles(i, *roles)
             await client.send_message(message.channel, 'Роли ' + ', '.join([i.mention for i in message.role_mentions]) + ' сняты с участников ' + ', '.join([i.mention for i in message.mentions]))
     
         if cmd.lower().startswith('help'):
-            embed=discord.Embed(title='Помощь по боту', color=0x008800)
+            embed=discord.Embed(title='Помощь по боту', color=0xff0000)
             embed.set_author(name="LGS бот")
             embed.add_field(name='`lgs.help`', value='Увидеть этот список', inline=True)
             embed.add_field(name='`lgs.say [каналы...] | <текст>`', value='Написать сообщение в `каналы...`', inline=True)
